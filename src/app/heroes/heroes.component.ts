0fb09d794c1bb6cff38e6026c331c00991aeca1e
Viktor Sheverdin <viktor.sheverdin@houle.ca>
2020-09-23 17:58:26 -0700

    Added route, need to finish it though

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,28 +2,41 @@ import { Component, OnInit } from '@angular/core';
 import { from } from 'rxjs';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../messages.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
+  // heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
-  // hero = {
-  //   name : 'Windstorm',
-  //   id : 1,
-  // }
 
-  constructor() { }
+  constructor(
+    private heroService: HeroService,
+    private MessageService: MessageService
+  ) {}
 
   ngOnInit(): void {
+    this.getHeroes();
   }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
-    console.log(this.selectedHero)
+    console.log(this.selectedHero);
+    this.MessageService.add(
+      `HeroComponent: Selected hero id is: ${this.selectedHero.id}, name is: ${this.selectedHero.name}`
+    );
   }
 
+  // getHeroes(): void {
+  //   this.heroes = this.heroService.getHeroes();
+  // }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+  }
 }
